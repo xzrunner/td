4bec133e9a6a28458f99f1006d3c9b1d6bfea7ea
zz <xzrunner@gmail.com>
2020-09-01 08:42:31 +0800

    rename

--- include/td/operator/Line.parm.h
+++ include/td/operator/Line.parm.h
@@ -2,6 +2,6 @@
 #error "You must define PARAM_INFO macro before include this file"
 #endif
 
-PARAM_INFO(PointA,    Float3, pa,     m_pa,     (0, 0, 0))
-PARAM_INFO(PointB,    Float3, pb,     m_pb,     (1, 0, 0))
-PARAM_INFO(PointsNum, Int,    points, m_points, (2))
+PARAM_INFO(PointA,    sm::vec3, pa,     m_pa,     (0, 0, 0))
+PARAM_INFO(PointB,    sm::vec3, pb,     m_pb,     (1, 0, 0))
+PARAM_INFO(PointsNum, int,      points, m_points, (2))
